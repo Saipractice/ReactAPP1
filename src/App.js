365bb892b7aee517dcc079d16654df01cad9c055
Sahan <kksahan78@gmail.com>
2025-06-20 20:47:19 +0530

    Added: react routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
+import Header from './Header';
+import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom';
+import Body from './Body';
+import AboutPage from './About';
+import ContactPage from './Contact';
+import Footer from './Footer';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />
+  },
+  {
+    path: "/about",
+    element: <AboutPage />
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <>
+      <Header />
+      <Link to="/about">About</Link>
+      <RouterProvider router={router}>
+        <div className="App">
+        </div>
+      </RouterProvider>
+      <Footer />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,36 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
-import Body from './Body';
-import AboutPage from './About';
-import ContactPage from './Contact';
-import Header from './Header';
-import Footer from './Footer';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Body />
-  },
-  {
-    path: "/about",
-    element: <AboutPage />
-  },
-  {
-    path: "/contact",
-    element: <ContactPage />
-  }
-])
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
-    <Footer />
+    <App/>
   </React.StrictMode>
 );
 
